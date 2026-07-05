61825b2e20016bd5d60fa4bb9005cdf2ea3fc2ad
YacaloX <172989715+YacaloX@users.noreply.github.com>
2026-07-04 23:11:46 -0500

    fix(gui): correct error/search list visibility with IsPositive converter

--- src/DiskGalaxy.UI/Converters/ValueConverters.cs
+++ src/DiskGalaxy.UI/Converters/ValueConverters.cs
@@ -11,6 +11,7 @@ public static class BoolConverters
 public static class ObjectConverters
 {
     public static readonly IValueConverter IsNotNull = new IsNotNullConverter();
+    public static readonly IValueConverter IsPositive = new IsPositiveConverter();
 }
 
 public sealed class NotConverter : IValueConverter
@@ -31,6 +32,19 @@ public sealed class IsNotNullConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is not null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+public sealed class IsPositiveConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is int i ? i > 0 : value is long l ? l > 0 : value is not null;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
